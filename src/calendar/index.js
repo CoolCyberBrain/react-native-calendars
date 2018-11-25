9b541cd2a1806a701a1a0531cbca93eebb1cd1b7
Calvin <me@calvin.sh>
2018-11-24 20:49:12 -0800

    Update index.js

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -100,7 +100,7 @@ class Calendar extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.theme !== this.props.theme)
-      this.style = styleConstructor(props.theme);
+      this.style = styleConstructor(nextProps.theme);
 
     const current= parseDate(nextProps.current);
     if (current && current.toString('yyyy MM') !== this.state.currentMonth.toString('yyyy MM')) {
